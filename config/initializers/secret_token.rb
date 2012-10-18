385066aab0ad7eedbb711fbba90c7102661315bd
Dan Mundy <drm.mundy@gmail.com>
2012-10-18 11:18:24 -0400

    Includes the output of RailsApps Composer

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CounselorTornado::Application.config.secret_token = '1e171f32aaa87f262f5913c203472aeedff8c9d36abf61c88b91d3af063914642dc2bca158331198cb321ae7d43b5b0985558b6647ec9ce52f23bcf9c7ce8ba9'
