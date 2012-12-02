0a383681c3e39cef29498dd451e0d5ccefe62904
Dan Mundy <drm.mundy@gmail.com>
2012-12-02 14:48:07 -0500

    Sets up Devise and follows its instructions

--- config/application.rb
+++ config/application.rb
@@ -66,5 +66,8 @@ module CounselorTornado
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Don't access DB when precompiling assets
+    config.assets.initialize_on_precompile = false
   end
 end
