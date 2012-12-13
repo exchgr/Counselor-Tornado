64e260e0b716eb0ffafde73ec51bf807f8099f53
Dan Mundy <drm.mundy@gmail.com>
2012-12-13 04:11:21 -0500

    Sets up assocs between user/counselor/appointment.

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,6 @@
 class Appointment < ActiveRecord::Base
   attr_accessible :blocks, :date_avail, :reason, :status
+
+  belongs_to :user
+  belongs_to :counselor
 end
--- app/models/counselor.rb
+++ app/models/counselor.rb
@@ -8,4 +8,7 @@ class Counselor < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me
   # attr_accessible :title, :body
+  
+  has_many :appointments
+  has_many :users, :through => :appointments
 end
