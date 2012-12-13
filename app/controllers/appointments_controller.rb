2e55c98ae96e138803551864205059eafd5dc28f
Dan Mundy <drm.mundy@gmail.com>
2012-12-13 08:01:18 -0500

    Starts apppointments/index, migrates foreign keys

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -2,6 +2,7 @@ class AppointmentsController < ApplicationController
   before_filter :authenticate_user!
 
   def index
+    @user = current_user
   end
 
   def show
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121213045100) do
+ActiveRecord::Schema.define(:version => 20121213125711) do
 
   create_table "admins", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -36,8 +36,10 @@ ActiveRecord::Schema.define(:version => 20121213045100) do
     t.string   "blocks"
     t.string   "date_avail"
     t.string   "status"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+    t.integer  "user_id"
+    t.integer  "counselor_id"
   end
 
   create_table "counselors", :force => true do |t|
